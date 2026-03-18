a907a1f929797f984704ebb4927082304de7e03b
mquan0802 <mquan080205@gmail.com>
2026-03-19 01:18:32 +0700

    sada
    dsađấ

--- src/main/java/model/Sach.java
+++ src/main/java/model/Sach.java
@@ -1,36 +1,66 @@
 package model;
-import java.util.Scanner;
 
 public class Sach extends SanPham {
+    private String tacGia;
+    private String theloai;
+    private int namXuatban;
+    private String nhaXuatBan;
 
-   private String theloai;
-   private int namXuatban;
-   //Tác giả 
-   //Lĩnh vực
+    public Sach() {
+    }
 
-   public Sach(String masp, String tensp, int soluong, double dongia,
-               String theloai, int namXuatban) {
-      super(masp, tensp, soluong, dongia);
-      this.theloai = theloai;
-      this.namXuatban = namXuatban;
-   }
+    public Sach(String masp, String tensp, int soluong, double dongia,
+                String tacGia, String theloai, int namXuatban, String nhaXuatBan) {
+        super(masp, tensp, soluong, dongia);
+        this.tacGia = tacGia;
+        this.theloai = theloai;
+        this.namXuatban = namXuatban;
+        this.nhaXuatBan = nhaXuatBan;
+    }
 
-   public String getTheloai() {
-      return theloai;
-   }
+    public String getTacGia() {
+        return tacGia;
+    }
 
-   public int getNamXuatban() {
-      return namXuatban;
-   }
+    public void setTacGia(String tacGia) {
+        this.tacGia = tacGia;
+    }
 
-   public void setNamXuatban(int namXuatban) {
-      this.namXuatban = namXuatban;
-   }
+    public String getTheloai() {
+        return theloai;
+    }
 
-   public void setTheloai(String theloai) {
-      this.theloai = theloai;
-   }
+    public void setTheloai(String theloai) {
+        this.theloai = theloai;
+    }
+
+    public int getNamXuatban() {
+        return namXuatban;
+    }
+
+    public void setNamXuatban(int namXuatban) {
+        this.namXuatban = namXuatban;
+    }
+
+    public String getNhaXuatBan() {
+        return nhaXuatBan;
+    }
+
+    public void setNhaXuatBan(String nhaXuatBan) {
+        this.nhaXuatBan = nhaXuatBan;
+    }
+
+    @Override
+    public String toString() {
+        return "Sach{" +
+                "masp='" + masp + '\'' +
+                ", tensp='" + tensp + '\'' +
+                ", soluong=" + soluong +
+                ", dongia=" + dongia +
+                ", tacGia='" + tacGia + '\'' +
+                ", theloai='" + theloai + '\'' +
+                ", namXuatban=" + namXuatban +
+                ", nhaXuatBan='" + nhaXuatBan + '\'' +
+                '}';
+    }
 }
-
-
-
--- src/main/java/model/VanPhongPham.java
+++ src/main/java/model/VanPhongPham.java
@@ -1,9 +1,55 @@
 package model;
 
 public class VanPhongPham extends SanPham {
-    public VanPhongPham(String masp, String tensp, int soluong, double dongia) {
-        super(masp, tensp, soluong, dongia);
+    private String thuongHieu;
+    private String xuatXu;
+    private String chatLuong;
+
+    public VanPhongPham() {
     }
 
+    public VanPhongPham(String masp, String tensp, int soluong, double dongia,
+                        String thuongHieu, String xuatXu, String chatLuong) {
+        super(masp, tensp, soluong, dongia);
+        this.thuongHieu = thuongHieu;
+        this.xuatXu = xuatXu;
+        this.chatLuong = chatLuong;
+    }
+
+    public String getThuongHieu() {
+        return thuongHieu;
+    }
+
+    public void setThuongHieu(String thuongHieu) {
+        this.thuongHieu = thuongHieu;
+    }
+
+    public String getXuatXu() {
+        return xuatXu;
+    }
+
+    public void setXuatXu(String xuatXu) {
+        this.xuatXu = xuatXu;
+    }
+
+    public String getChatLuong() {
+        return chatLuong;
+    }
+
+    public void setChatLuong(String chatLuong) {
+        this.chatLuong = chatLuong;
+    }
+
+    @Override
+    public String toString() {
+        return "VanPhongPham{" +
+                "masp='" + masp + '\'' +
+                ", tensp='" + tensp + '\'' +
+                ", soluong=" + soluong +
+                ", dongia=" + dongia +
+                ", thuongHieu='" + thuongHieu + '\'' +
+                ", xuatXu='" + xuatXu + '\'' +
+                ", chatLuong='" + chatLuong + '\'' +
+                '}';
+    }
 }
-// Thương hiệu
